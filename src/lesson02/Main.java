008a30f946c559c1f48dcc488c70c86ecee6cf06
AbrorMC <91465869+AbrorMC@users.noreply.github.com>
2025-09-26 12:40:47 +0500

    9 tasks are done!

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,20 +1,36 @@
-package lesson02;
-
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Task number 1:\n");
         numbersFromOneToHundred();
 
         System.out.println("Task number 2:\n");
-        int number = getInput();
-        sumOfNumbersFromOneToN(number);
+        sumOfNumbers(getInput());
+
+        System.out.println("Task number 3 and 7:\n");
+        factorial(getInput());
+
+        System.out.println("Task number 4:\n");
+        sumOfOddNumbers(getInput());
+
+        System.out.println("Task number 5:\n");
+        sumOfDigits(getInput());
+
+        System.out.println("Task number 6:\n");
+        turnNumber(getInput());
+
+        System.out.println("Task number 8:\n");
+        findDivisibleToSeven();
+
+        System.out.println("Task number 9:\n");
+        printPrimeNumbers(getInput());
     }
 
     public static int getInput() {
         System.out.println("Please, enter number");
-        Scanner scanner = new Scanner(System.in);
         int result;
         while (true) {
             if (scanner.hasNextInt()) {
@@ -25,32 +41,103 @@ public class Main {
                     break;
                 }
             } else {
-                System.out.println("Please, enter number");
+                System.out.println("Please, enter valid number");
                 scanner.next();
             }
         }
-        scanner.close();
         return result;
     }
+    
     public static void numbersFromOneToHundred() {
         int number = 1;
         while (number <= 100) {
-            if (number > 1 && number % 10 == 1)
-                System.out.print('\n');
+            System.out.print(number);
+            if (number % 10 == 0)
+                System.out.print("\n");
             else
-                System.out.print(number + " ");
+                System.out.print(" ");
             number++;
         }
         System.out.println("\n");
     }
 
-    public static void sumOfNumbersFromOneToN(int lastNumber) {
-        int result = 0, number = 0;
-        while (number <= lastNumber) {
-            result += number;
+    public static void sumOfNumbers(int number) {
+        int result = 0, curNumber = 0;
+        while (curNumber <= number) {
+            result += curNumber;
+            curNumber++;
+        }
+        System.out.println(result + "\n");
+    }
+
+    public static void factorial(int number) {
+        int result = 1, curNumber = 1;
+        while (curNumber <= number) {
+            result *= curNumber;
+            curNumber++;
+        }
+        System.out.println(result + "\n");
+    }
+
+    public static void sumOfOddNumbers(int number) {
+        int result = 0, curNumber = 0;
+        while (curNumber <= number) {
+            if (curNumber % 2 == 0)
+                result += curNumber;
+            curNumber++;
+        }
+        System.out.println(result + "\n");
+    }
+    
+    public static void sumOfDigits(int number) {
+        int result = 0, digit = 0;
+        while (number > 0) {
+            digit = number % 10;
+            number /= 10;
+            result += digit;
+        }
+        System.out.println(result + "\n");
+    }
+    
+    public static void turnNumber(int number) {
+        int result = 0, digit = 0;
+        while (number > 0) {
+            digit = number % 10;
+            number /= 10;
+            result = result * 10 + digit;
+        }
+        System.out.println(result + "\n");
+    }
+    
+    public static void findDivisibleToSeven() {
+        int result = 0, digit = 0, number = 1000;
+        while (true) {
+            digit = number % 7;
+            if (digit == 0) {
+                result = number;
+                break;
+            }
             number++;
         }
-        System.out.println(result);
+        System.out.println(result + "\n");
+    }
+    
+    public static void printPrimeNumbers(int number) {
+        int curNumber = 2;
+        while (curNumber < number) {
+            if (isPrime(curNumber))
+                System.out.print(curNumber + " ");
+            curNumber++;
+        }
+        System.out.println("\n");
+    }
+    
+    private static Boolean isPrime(int number) {
+        for (int i = 2; i < Math.sqrt(number) + 1; i++) {
+            if (number % i == 0 && number != 2)
+                return false;
+        }
+        return true;
     }
 }
 
